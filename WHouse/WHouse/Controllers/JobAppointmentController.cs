e83aaf231860191b02050de0ec3583b26826b7f3
Alauskas <37072295+Alauskas@users.noreply.github.com>
2019-05-19 19:18:45 +0300

    kazkas
    kazkas

---
+++ WHouse/WHouse/Controllers/JobAppointmentController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WHouse.Models;
+
+namespace WHouse.Controllers
+{
+    public class JobAppointmentController : Controller
+    {
+        private MydataEntities db = new MydataEntities();
+        // GET: JobAppointment
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Start(int id)
+        {
+            
+
+
+
+
+
+
+            var orderProducts = db.OrderProducts.Include(o => o.CustumerOrder).Include(o => o.Inventory);
+            return View(orderProducts.ToList());
+        }
+        public void SelectFreeWorkers()
+        {
+           // var freeworkers = db.Userrs.Select();
+           // return View(inventories.ToList());
+
+        }
+    }
+}
